85585538eab1247ce1db1fa20d218c7fe1a3e5fe
Jose J. Cruz <jjzcru@gmail.com>
2021-08-06 12:02:07 -0400

    feat: :sparkles: Use canvasflow config file

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
 #!/usr/bin/env node
-'use strict'
-require('dotenv').config()
+'use strict';
+const fs = require('fs');
+const envPath = process.env.CANVASFLOW_CONFIG;
 
-const server = require('./server')
-const signale = require('./utils/signale')
-const config = require('./utils/config')
-const connect = require('./utils/connect')
-const stripUrlAuth = require('./utils/stripUrlAuth')
-
-if (config.dbUrl == null) {
-	signale.fatal('MongoDB connection URI missing in environment')
-	process.exit(1)
+// Load env variables
+if (envPath) {
+	if (fs.existsSync(envPath)) {
+		// tslint:disable-next-line:no-var-requires
+		require('dotenv').config({ path: envPath });
+	}
 }
 
-server.on('listening', () => signale.watch(`Listening on http://localhost:${ config.port }`))
-server.on('error', (error) => signale.fatal(error))
+const server = require('./server');
+const signale = require('./utils/signale');
+const config = require('./utils/config');
+const connect = require('./utils/connect');
+const stripUrlAuth = require('./utils/stripUrlAuth');
 
-signale.await(`Connecting to ${ stripUrlAuth(config.dbUrl) }`)
+console.log(`CONFIG`);
+console.log(config);
 
-connect(config.dbUrl).then(() => {
-	signale.success(`Connected to ${ stripUrlAuth(config.dbUrl) }`)
-	signale.start(`Starting the server`)
+if (config.dbUrl == null) {
+	signale.fatal('MongoDB connection URI missing in environment');
+	process.exit(1);
+}
 
-	server.listen(config.port)
+server.on('listening', () =>
+	signale.watch(`Listening on http://localhost:${config.port}`)
+);
+server.on('error', (error) => signale.fatal(error));
 
-	if (config.isDevelopmentMode === true) {
-		signale.info('Development mode enabled')
-	}
+signale.await(`Connecting to ${stripUrlAuth(config.dbUrl)}`);
 
-	if (config.isDemoMode === true) {
-		signale.info('Demo mode enabled')
-	}
-})
-	.catch((error) => {
-		signale.fatal(error)
-		process.exit(1)
+connect(config.dbUrl)
+	.then(() => {
+		signale.success(`Connected to ${stripUrlAuth(config.dbUrl)}`);
+		signale.start(`Starting the server`);
+
+		server.listen(config.port);
+
+		if (config.isDevelopmentMode === true) {
+			signale.info('Development mode enabled');
+		}
+
+		if (config.isDemoMode === true) {
+			signale.info('Demo mode enabled');
+		}
 	})
+	.catch((error) => {
+		signale.fatal(error);
+		process.exit(1);
+	});
